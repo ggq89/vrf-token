e7334492d2f62f3179b5e8cbaa88b593470660c2
ggq89 <ggq89@qq.com>
2021-05-08 15:15:23 +0800

    add deploy.ts

---
+++ scripts/deploy.ts
@@ -0,0 +1,30 @@
+import { Contract, ContractFactory } from "ethers";
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+// When running the script with `hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+import { ethers, upgrades } from "hardhat";
+
+async function main(): Promise<void> {
+  // Hardhat always runs the compile task when running scripts through it.
+  // If this runs in a standalone fashion you may want to call compile manually
+  // to make sure everything is compiled
+  // await run("compile");
+
+  // We get the contract to deploy
+  const VrfToken: ContractFactory = await ethers.getContractFactory("VrfToken");
+  const vrfToken: Contract = await upgrades.deployProxy(VrfToken);
+  // const vrfToken = await VrfToken.deploy();
+  await vrfToken.deployed();
+
+  console.log("VrfToken Proxy deployed to: ", vrfToken.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error: Error) => {
+    console.error(error);
+    process.exit(1);
+  });
